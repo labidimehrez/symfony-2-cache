89341125f75971c818644b048a53e95a194eb062
mehrez.labidi <mehrez.labidi@tritux.com>
2018-03-28 16:14:46 +0200

    ESI

--- src/MyBundle/Controller/ExampleController.php
+++ src/MyBundle/Controller/ExampleController.php
@@ -1,24 +1,30 @@
 <?php
 
-namespace MyBundle\Controller;
 
+namespace MyBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class ExampleController extends Controller {
 
     public function cacheAction() {
-        
-        $response = new Response();
-        $response->setMaxAge(300);
-        // Check that the Response is not modified for the given Request
-        if (!$response->isNotModified($this->getRequest())) {
-            $date = new \DateTime();
-            $response->setLastModified($date);
-            $response->setContent($this->renderView('MyBundle:Cache:cache.html.twig', array('hello' => 'Hello World!!!')));
-        }
-
-        return $response;
+        return $this->render('MyBundle:Cache:cache.html.twig',
+            array('hello' => 'Hello World!!!'));
     }
 
+    public function esiAction(Request $request) {
+        $response = new Response();
+        $response->setSharedMaxAge(10);
+        $response->setPublic();
+        $response->setMaxAge(10);
+
+        // Check that the Response is not modified for the given Request
+        if (!$response->isNotModified($request)) {
+            $date = new \DateTime();
+            $response->setLastModified($date);
+            $response->setContent($this->renderView('MyBundle:Cache:esi.html.twig'));
+        }
+        return $response;
+    }
 }
